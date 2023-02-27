ba7eb19db634663602f1a2460b5a05e62ab2d5f9
cdawg3 <cdawg4604@gmail.com>
2023-02-27 11:05:58 -0600

    finalized version

---
+++ index.js
@@ -0,0 +1,62 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+// localStorage.clear()
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+} else {
+  console.log("No leads from local storage");
+}
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a href='${leads[i]}' target='_blank'>
+            ${leads[i]}
+        </a>
+        <button id='btn' onclick='deleteLead()'>X</button>
+      </li>
+      `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+  // console.log(localStorage.getItem("myLeads"))
+});
+
+tabBtn.addEventListener("click", function () {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//   });
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs)
+         myLeads.push(tabs[0].url)
+         localStorage.setItem("myLeads", JSON.stringify(myLeads))
+         render(myLeads)
+    })
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+function deleteLead() {
+  console.log("lead deleted");
+}
